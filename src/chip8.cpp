646acd90b8b8c97e154a37bbc0c6c94702bb539b
Jejis06 <wertex3233@gmail.com>
2023-01-01 18:06:58 +0100

    make debug for chip8.cpp readable

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -469,6 +469,7 @@ void Chip8::Cycle(){
 	pc += 2;
 	
 	((*this).*(ITable[(opc & 0xF000u) >> 12u]))();
+
 	if(debug) 
 		std::cout << std::hex \
 			<< "Opcode : " << opc \
@@ -476,7 +477,11 @@ void Chip8::Cycle(){
 			<< " | Linker : "<< (opc & 0x000Fu)\
 			<< " | Linker(2) : "<<(opc & 0x00FFu)\
 		<<'\n';
-	if(delay > 0) delay--;
-	if (soundTimer > 0) soundTimer--;
+
+	if(delay > 0) 
+		delay--;
+
+	if (soundTimer > 0) 
+		soundTimer--;
 }
 
